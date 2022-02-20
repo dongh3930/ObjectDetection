b344ce94eca0dd3a3cb112d845bf1341bd4d913f
dongh3930 <dongh3930@naver.com>
2022-02-20 21:53:05 +0900

    commit log

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while(cap.isOpened()):
     ret, frame = cap.read()
 
     # apply object detection (물체 검출)
-    bbox, label, conf = cv.detect_common_objects(frame)
+    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov4-tiny')
 
     # draw bounding box over detected objects (검출된 물체 가장자리에 바운딩 박스 그리기)
     out = draw_bbox(frame, bbox, label, conf, write_conf=True)
